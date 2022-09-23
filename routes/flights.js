3b5100ef9675eb0fec81aa56c88ef2cc9188047d
Carol Kang <otherhats@gmail.com>
2022-09-23 16:53:44 -0400

    fix typos on model

--- routes/flights.js
+++ routes/flights.js
@@ -1,11 +1,8 @@
 import { Router } from 'express'
-
+import * as flightsCtrl from '../controllers/flights.js'
 const router = Router()
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource')
-})
+router.get('/', flightsCtrl.index)
 
 export {
   router
